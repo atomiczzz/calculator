0ad2083c04b5bd0a755e00106a465abc9b7dab99
David Robicheaux <davidrobicheaux@Davids-MacBook-Pro.local>
2017-05-14 00:08:08 -0500

    able to perform basic calculation

--- main.js
+++ main.js
@@ -1,27 +1,48 @@
 let $ = window.$
-let numStore = []
-let operatorStore = []
-let display = document.querySelector('#display')
-
-// Get data attr and push to numStore array
 $(document).ready(function () {
+  let numStore = []
+  let display = document.querySelector('#display')
+
+  // Get data attr and push to numStore array
   $('.calc_button').click(function () {
     numStore.push(this.dataset.num)
-    let display = document.querySelector('#display')
     display.innerHTML += this.dataset.num
     console.log(numStore)
   })
-})
-// Get operator and push to operatorStore
-$(document).ready(function () {
+
+  // Get operator and push to operatorStore
   $('.op_button').click(function () {
-    operatorStore.push(this.dataset.num)
-    let display = document.querySelector('#display')
+    numStore.push(this.dataset.num)
     display.innerHTML += this.dataset.num
-    console.log(operatorStore)
   })
+
+  // Equal button on click activates funcions
+  $('#equal').click(function () {
+    operator()
+    display.innerHTML = operator()
+  })
+
+  $('#clear').click(function () {
+    numStore = ['']
+    display.innerHTML = '0'
+  })
+
+  // perator functions
+  let operator = function () {
+    let sum = numStore.reduce(function (accum, currentOp, index, arr) {
+      let total = parseFloat(arr[index + 1])
+      accum = parseFloat(accum)
+      if (currentOp === '+') {
+        accum += total
+      } else if (currentOp === '-') {
+        accum -= total
+      } else if (currentOp === '*') {
+        accum *= total
+      } else if (currentOp === '/') {
+        accum /= total
+      }
+      return accum
+    }, numStore[0])
+    return sum
+  }
 })
-// Equal button on click activates funcions
-
-
-// Operator functions
